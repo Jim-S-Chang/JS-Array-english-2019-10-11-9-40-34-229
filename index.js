12d953944503067846c6e25edfc1e2975323c863
Jim-S-Chang <jim.s.chang@qq.com>
2020-06-15 23:14:21 +0800

    finish practice

--- index.js
+++ index.js
@@ -2,25 +2,71 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(typeof(a))
+console.log(typeof(b))
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+for (let i = 0; i < a.length; i++) {
+    a[i] *= 2;
+}
+console.log(a)
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+let case1 = '';
+let case2 = '';
+let case3 = '';
+for (let i = 0; i < colors.length; i++) {
+    case1 += colors[i];
+    if (i !== colors.length - 1) {
+        case1 += ' ';
+    }
+}
+for (let i = 0; i < colors.length; i++) {
+    case2 += colors[i];
+    if (i !== colors.length - 1) {
+        case2 += '+';
+    }
+}
+for (let i = 0; i < colors.length; i++) {
+    case3 += colors[i];
+    if (i !== colors.length - 1) {
+        case3 += ',';
+    }
+}
+console.log(case1);
+console.log(case2);
+console.log(case3);
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+function compare(value1, value2) {
+    return value2 - value1;
+};
+a = a.sort(compare)
+console.log(a)
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var resulteArr = [];
+a.forEach(item => {
+    var count = 0;
+    a.forEach(hash => {
+        if (item === hash) {
+            count++;
+        }
+    });
+    resulteArr.push(count);
+})
+var max = Math.max.apply(null, resulteArr);
+var index = resulteArr.findIndex(item => item === max);
+console.log(a[index])
